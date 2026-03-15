799f249fc324bb1bbe4aa1211c8cd4d69a16f98a
Kashish <kashish.valorous@gmail.com>
2026-03-15 17:10:05 +0530

    Added UC2: Display OOPS banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,19 +1,27 @@
-/**
- * OOPS Banner App
- * UC1: Print OOPS to Console
- * * Goal: Display the literal text "OOPS" (Object-Oriented Programming System) 
- * once on the console.
- * * @author Technical Team
- * @version 1.0
- */
 public class OOPSBannerApp {
 
-    /**
-     * Main method - Entry point of the application.
-     * @param args Command line arguments
-     */
     public static void main(String[] args) {
-        // UC1: Simple print statement using String Literal
-        System.out.println("OOPS");
+
+        // Line 1
+        System.out.println("  *****    *****    ******    *****  ");
+
+        // Line 2
+        System.out.println(" *     *  *     *   *     *  *     * ");
+
+        // Line 3
+        System.out.println(" *     *  *     *   *     *  *       ");
+
+        // Line 4
+        System.out.println(" *     *  *     *   ******    *****  ");
+
+        // Line 5
+        System.out.println(" *     *  *     *   *              * ");
+
+        // Line 6
+        System.out.println(" *     *  *     *   *        *     * ");
+
+        // Line 7
+        System.out.println("  *****    *****    *         *****  ");
+
     }
 }
